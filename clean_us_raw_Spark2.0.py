82a3eaa509aeba69cab4ef02265d60ccc6dca69e
hanhan <wuhanhan999@gmail.com>
2016-07-28 23:39:20 -0700

    update clean_us_raw_Spark2.0.py

--- clean_us_raw_Spark2.0.py
+++ clean_us_raw_Spark2.0.py
@@ -1,7 +1,6 @@
 # Note, this is using Spark 2.0 on Spark Python Notebook, some features are unique here
 
 # cell 1
-
 %sql
 -- import US_raw.txt into Spark Cloud first
 
@@ -12,6 +11,5 @@ OPTIONS (path "[your HDFS path]/US_raw.txt", header "true", delimiter "\t")
 
 # cell 2
 ## This is the new change in Spark 2.0! Much more convenient!!
-
 us_code_df = spark.sql("select distinct Name as US_City, SubDiv as US_STATE from US_Code")
 us_code_df.show(n = 3)
