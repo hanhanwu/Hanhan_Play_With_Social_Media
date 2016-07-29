0697e01decbc17657d667ada7a97701a9ec88f3c
hanhan <wuhanhan999@gmail.com>
2016-07-28 21:53:58 -0700

    update Pokemon_Yelp_Explore.py

--- Pokemon_Yelp_Explore.py
+++ Pokemon_Yelp_Explore.py
@@ -1,4 +1,4 @@
-# cell 1
+# cell 1 - crtea Yelp client based on your keys
 
 from yelp.client import Client
 from yelp.oauth1_authenticator import Oauth1Authenticator
@@ -13,13 +13,50 @@ auth = Oauth1Authenticator(
 client = Client(auth)
 
 
-# cell 2
+# cell 2 - TYR1: try to find whether Yelp search is based on key words in current business content
+
+params = {
+    'term': 'Pokemon'
+}
+response = client.search('Seattle', **params)
+
+ct = 0
+for biz in response.businesses:
+  print biz.name
+  print biz.id
+  print biz.image_url
+  print biz.categories
+  print biz.rating
+  print biz.reviews
+  
+  print biz.location.address
+  print biz.location.display_address  # full address
+  print biz.location.cross_streets
+  print biz.location.neighborhoods
+  print biz.location.coordinate.latitude
+  print biz.location.coordinate.longitude
+  
+  print biz.deals
+  print biz.gift_certificates
+  print biz.menu_provider
+  print biz.reservation_url
+  print biz.eat24_url
+  print biz.reviews
+  
+  print biz.snippet_text
+  print '********************'
+  
+  ct += 1
+  if ct == 2: break
+
+
+
+# cell 3 - TRY2: try to see whether 'Pokemon Go' and 'Pokemon' will find similar business categories
 
 import operator
 
 category_dct = {}
 US_search_city_lst = ['Seattle', 'Redmond', 'Bellevue']
-snippet_text_set = set()
 my_term = 'Pokemon Go'
 
 params = {
@@ -30,8 +67,6 @@ for us_city in US_search_city_lst:
   us_response = client.search(us_city, **params)
   for biz in us_response.businesses:
     category_lst = [e[1] for e in biz.categories]
-    if biz.snippet_text != None:
-      snippet_text_set.add(biz.snippet_text)
     for c in category_lst:
       category_dct.setdefault(c, 0)
       category_dct[c] += 1
@@ -40,21 +75,9 @@ sorted_category_lst = sorted(category_dct.items(), key=operator.itemgetter(1), r
 
 for itm in sorted_category_lst:
   print itm
-  
-print
-for t in list(snippet_text_set):
-  if my_term.lower() in t.lower():
-    print '*****',t
-    print
     
+##***************************##
 
-# cell 3
-
-import operator
-
-category_dct = {}
-US_search_city_lst = ['Seattle', 'Redmond', 'Bellevue']
-snippet_text_set = set()
 my_term = 'Pokemon'
 
 params = {
@@ -65,8 +88,6 @@ for us_city in US_search_city_lst:
   us_response = client.search(us_city, **params)
   for biz in us_response.businesses:
     category_lst = [e[1] for e in biz.categories]
-    if biz.snippet_text != None:
-      snippet_text_set.add(biz.snippet_text)
     for c in category_lst:
       category_dct.setdefault(c, 0)
       category_dct[c] += 1
@@ -75,10 +96,3 @@ sorted_category_lst = sorted(category_dct.items(), key=operator.itemgetter(1), r
 
 for itm in sorted_category_lst:
   print itm
-  
-print
-for t in list(snippet_text_set):
-  print t
-  if my_term.lower() in t.lower():
-    print '*****',t
-  print
