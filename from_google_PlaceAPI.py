1d37d77464c77cbba644b6b10b88f2ea3482df0a
hanhan <wuhanhan999@gmail.com>
2016-09-18 13:27:51 -0700

    update from_google_PlaceAPI.py

--- from_google_PlaceAPI.py
+++ from_google_PlaceAPI.py
@@ -46,3 +46,59 @@ for place in query_result.places:
   print "Open Now: ", p_details["opening_hours"]["open_now"]
   
   break
+
+
+
+# cell 2
+# convert all the data into df, so that it's easier to do Spark sql operation
+
+from datetime import datetime
+from pytz import timezone
+import pytz
+from pyspark.sql import Row
+
+
+palces_lst = []
+local_tz = timezone('US/Pacific')
+Merchant = Row("Name", "Open_Now", "Address", "Phone", "Website", "Rating", "Comment", "Rating_Time")
+merchant_lst = []
+
+for place in query_result.places:
+  place.get_details()
+  p_details = place.details
+  
+  p_name = place.name
+  p_web = place.website
+  p_addr = p_details["formatted_address"]
+  if "formatted_phone_number" in p_details.keys():
+    p_phone = p_details["formatted_phone_number"]
+  else:
+    p_phone = None
+  p_open_now = p_details["opening_hours"]["open_now"]
+  
+  for r in p_details["reviews"]:
+    r_rating = r["rating"]
+    r_text = r["text"]
+    t_time = datetime.fromtimestamp(r["time"]).replace(tzinfo=pytz.utc).astimezone(local_tz).strftime('%Y-%m-%d, %H:%M:%S, PST')
+    merchant_lst.append(Merchant(p_name, p_open_now, p_addr, p_phone, p_web, r_rating, r_text, t_time))
+  
+  
+
+# cell 3
+df = sc.parallelize(merchant_lst).toDF()
+df.show()
+
+
+
+# cell 4
+df.registerTempTable("merchant_table")
+
+
+
+# cell 5 - Spark SQL, Now I want to know top recommended bubble tea store near Redmond :)
+%sql
+
+select Name, Address, avg(Rating) as Avg_Rating
+from merchant_table
+group by Name, Address
+order by avg(Rating) desc
