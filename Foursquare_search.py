69853d2ddd3c225a44a4706299f7c8aa259fc0bc
hanhan <wuhanhan999@gmail.com>
2016-09-22 19:42:23 -0700

    update Foursquare_search.py

--- Foursquare_search.py
+++ Foursquare_search.py
@@ -12,7 +12,7 @@ print al1
 
 
 
-# cell 1 - search request
+# cell 1 - search venues
 import requests 
 
 ids = "client_id=" + CLIENT_ID + "&client_secret=" + CLIENT_SECRET
@@ -24,6 +24,32 @@ print search_url
 r = requests.post(search_url)
 print r.status_code
 print r.json()
+
+
+
+# cell 2 - formatted venue
+json_output = r.json()
+all_venues = json_output['response']['venues']
+
+for ve in all_venues:
+  print ve['name']
+  
+  print 'here now: ', ve['hereNow']['count']
+  print ve['hereNow']['summary']
+  print 'specials: ', ve['specials']
+  
+  print 'distance: ', ve['location']['distance']
+  print ve['location']['formattedAddress']
+  print str(ve['location']['lat']) + ', ' + str(ve['location']['lng'])
+  
+  print 'tip count: ', ve['stats']['tipCount']
+  print 'checkin count: ', ve['stats']['checkinsCount']
+  
+  print 'venue id: ', ve['id']
+  print 'Categories: '
+  for c in ve['categories']:
+    print '  (', c['name'], ', ', c['id'], ')'
+  break
 
 
 # TO BE CONTINUED....
